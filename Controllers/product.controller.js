058ed1698c6a9fa60168a7f2d5a0ce2a2c5881ba
rahul <rj7407@gmail.com>
2019-06-24 06:42:10 -0700

    last

--- Controllers/product.controller.js
+++ Controllers/product.controller.js
@@ -1,9 +1,12 @@
-const Product = require('../Models/product.model');
+
+
+const Product = require('../models/product.model');
 
 //Simple version, without validation or sanitation
 exports.test = function (req, res) {
     res.send('Greetings from the Test controller!');
 };
+
 exports.product_create = function (req, res) {
     let product = new Product(
         {
@@ -23,6 +26,7 @@ exports.product_create = function (req, res) {
 exports.product_details = function (req, res) {
     Product.findById(req.params.id, function (err, product) {
         if (err) return next(err);
+        res.send('something going wrong')
         res.send(product);
     })
 };
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const product = require('.product.route'); // Imports routes for the products
+const product = require('./routes/product.route'); // Imports routes for the products
 const app = express();
 app.use('/products', product);
 let port = 1234;
@@ -8,16 +8,20 @@ app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
 });
 
+
+
+
 const mongoose = require('mongoose');
 let dev_db_url = 'mongodb://accountUser:password@localhost:27017/monday';
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB);
+mongoose.connect(mongoDB , {useNewUrlParser:true});
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use('/products', product);
+
+
 
 
 
