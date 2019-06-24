b37a97bf7760fb2b564ab19887d8cd8535cc98d2
rahul <rj7407@gmail.com>
2019-06-24 03:49:04 -0700

    first

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const product = require('./routes/product.route'); // Imports routes for the products
 const app = express();
+app.use('/products', product);
 let port = 1234;
-
-
-
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
 });
 
-
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://accountUser:password@localhost:27017/monday';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
--- routes/product.route.js
+++ routes/product.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Require the controllers WHICH WE DID NOT CREATE YET!!
-const product_controller = require('../controllers/product.controller');
+const product_controller = require('..product.controller');
 
 
 // a simple test url to check that all of our files are communicating correctly.
